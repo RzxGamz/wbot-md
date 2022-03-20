9486126b31553f77b9bd3c3d24cad550e03546f5
ArveGanz <97107434+RzxGamz@users.noreply.github.com>
2022-03-20 11:54:19 +0700

    Add files via upload

---
+++ command/misc/status.js
@@ -0,0 +1,16 @@
+const os = require('os');
+const { formatSize } = require("../../utils");
+
+module.exports = {
+    name: 'stats',
+    alias: ['status'],
+    category: 'misc',
+    desc: 'Bot Stats',
+    async exec(msg, sock) {
+        let text = ''
+        text += `HOST:\n- Arch: ${os.arch()}\n- CPU: ${os.cpus()[0].model}${os.cpus().length > 1 ? (' (' + os.cpus().length + 'x)') : ''}\n- Release: ${os.release()}\n- Version: ${os.version()}\n`
+        text += `- Memory: ${formatSize(os.totalmem() - os.freemem())} / ${formatSize(os.totalmem())}\n`
+        text += `- Platform: ${os.platform()}`;
+        await sock.sendMessage(msg.from, { text }, { quoted: msg });
+    }
+}
